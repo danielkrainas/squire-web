e753a59c0f9f7b0d7cc2ede6688ddda461c1e05d
dkrainas87@gmail.com <dkrainas87@gmail.com>
2014-03-27 13:06:10 -0400

    added missing PreAppStartMethod assembly attribute.

--- src/Squire.Web/Properties/AssemblyInfo.cs
+++ src/Squire.Web/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
-﻿using System.Reflection;
+﻿using Squire.Web;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Web;
 
 [assembly: AssemblyTitle("Squire.Web")]
 [assembly: AssemblyDescription("")]
+[assembly: PreApplicationStartMethod(typeof(PreApplicationStartCode), "Start")]

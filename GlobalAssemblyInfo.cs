46fa200f1d6c57773ecb9eaced311d1bc30bcbab
dkrainas87@gmail.com <dkrainas87@gmail.com>
2014-03-27 13:38:25 -0400

    added task to update version info easier.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -5,9 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Daniel Krainas")]
 [assembly: AssemblyProduct("Squire Framework")]
-[assembly: AssemblyCopyright("Copyright © Squire Team 2014")]
+[assembly: AssemblyCopyright("Copyright (C) Squire Team 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.1.4.0")]
-[assembly: AssemblyFileVersion("0.1.4.0")]
+[assembly: AssemblyVersion("0.1.4")]

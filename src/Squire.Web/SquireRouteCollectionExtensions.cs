e5f07e43b7f12e7aa70bb1c8b9c0d0d461cc6631
dkrainas87@gmail.com <dkrainas87@gmail.com>
2014-05-23 00:41:43 -0400

    added helper for registering common home route

---
+++ src/Squire.Web/SquireRouteCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Squire.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    public static class SquireRouteCollectionExtensions
+    {
+        public static void MapHome(this RouteCollection routes, string url = "/", string controller = "Home", string action = "Index",  string[] namespaces = null)
+        {
+            routes.MapRoute(
+                name: CommonRoutes.Home,
+                url: url,
+                namespaces: namespaces,
+                defaults: new { controller = controller, action = action });
+        }
+    }
+}

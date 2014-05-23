363dd9d1fe75f998a2034aa73a6be38142e40779
dkrainas87@gmail.com <dkrainas87@gmail.com>
2014-05-23 00:35:06 -0400

    added parameter to specify namespaces of route controllers.

--- src/Squire.Web.Security/RouteCollectionExtensions.cs
+++ src/Squire.Web.Security/RouteCollectionExtensions.cs
@@ -10,28 +10,32 @@
 
     public static class RouteCollectionExtensions
     {
-        public static void MapAccountRoutes(this RouteCollection routes, string controllerName = "Account", string controllerUrlPath = "account/")
+        public static void MapAccountRoutes(this RouteCollection routes, string controllerName = "Account", string controllerUrlPath = "account/", string[] namespaces = null)
         {
             controllerName.VerifyParam("controllerName").IsNotNull();
             controllerUrlPath.VerifyParam("controllerUrlPath").IsNotNull();
             routes.MapRoute(
                 name: AccountRoutes.Login,
                 url: controllerUrlPath + "login",
+                namespaces: namespaces,
                 defaults: new { controller = controllerName, action = "Login" });
 
             routes.MapRoute(
                 name: AccountRoutes.Logout,
                 url: controllerUrlPath + "logout",
+                namespaces: namespaces,
                 defaults: new { controller = controllerName, action = "Logout" });
 
             routes.MapRoute(
                 name: AccountRoutes.Profile,
                 url: controllerUrlPath,
+                namespaces: namespaces,
                 defaults: new { controller = controllerName, action = "Index" });
 
             routes.MapRoute(
                 name: AccountRoutes.Registration,
                 url: controllerUrlPath + "register",
+                namespaces: namespaces,
                 defaults: new { controller = controllerName, action = "Register" });
         }
     }
